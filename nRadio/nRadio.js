eb98f69632fb9ce038fd2cd20624da1b934a9331
Martin0417 <18767122887@163.com>
2014-12-07 18:43:47 +0800

    nRadio完善——Mating

--- nRadio/nRadio.js
+++ nRadio/nRadio.js
@@ -3,7 +3,7 @@
  * @param  [original elem,original elem] or jQuery elem
  * @return null
  * 对外接口
- * 1.$.nRadio.init(jQelem) //初始化，装饰原始radio
+ * 1.$.nRadio.init(jQelem) //初始化，装饰原始radio, 建议 $.nRadio.init($("input:radio"))
  * 2.$.nRadio.check(jQelem) //jQelem为radio或radio的父标签a(待实现)
  * 2014-11-30   马厅  hzmating
  */
@@ -26,23 +26,42 @@
             return $(radios);
         };
         this.makenRadio = function($radios){
-            //wrap radios
+            //wrap radios first
             $radios.each($.proxy(function(num, radio){
-                $(radio).wrap($(radio.checked ? "<a href='javascript:;' class='wrapnRadio checked'></a>":"<a href='javascript:;' class='wrapnRadio unchecked'></a>"));
-                //bind event
+                $(radio).wrap($("<a href='javascript:;' class='wrapnRadio "+ (radio.checked ? "nRadioChecked" : "nRadioUnchecked") +"'></a>"));
+            },this));
+            //then bind event
+            $radios.each($.proxy(function(num, radio){
                 this.bindEvent($(radio),$radios);
             },this));
         };
         this.bindEvent = function($radio,$radios){
-            $radio.parent().click(function(){
-                var $this = $(this), checked = $this.hasClass("checked");
-                if(checked)
+            var $a = $radio.parent(),
+                $radioGroup =  $radios.filter("[name="+$radio[0].name+"]"),
+                $label = $(document).find("label[for="+$radio.attr("id")+"]");
+            //bind a
+            $a.click(function(){
+                if($a.hasClass("nRadioChecked"))
                     return;
-                // $this.toggleClass("checked", !checked).toggleClass("unchecked", checked);
-                var $a = $radios.filter("[name="+$radio[0].name+"]").parent(".checked");
-                $a.removeClass("checked").addClass("unchecked").find("input").attr("checked",false);
-                $this.removeClass("unchecked").addClass("checked").find("input").attr("checked",true);
+                if($label.length){
+                    $label.click();
+                }else{
+                    updateStatus($a, $radioGroup);
+                }
             });
+            //bind label  ,search by label[for=id]
+            if($label.length){
+                $label.click(function(){
+                    if(!$a.hasClass("nRadioChecked")){
+                        //坑：label也要设置input的checked状态
+                        updateStatus($a, $radioGroup);
+                    }
+                });
+            }
+            function updateStatus($a, $radioGroup){
+                $radioGroup.parent(".nRadioChecked").removeClass("nRadioChecked").addClass("nRadioUnchecked").find("input").attr("checked",false);
+                $a.removeClass("nRadioUnchecked").addClass("nRadioChecked").find("input").attr("checked",true);
+            }
         };
         this.check = function(){
             alert("sorry!not complete!");
